e438023092f85b60143882f043767d27935f8653
Antonio Furtado <antoniordf@hotmail.com>
2023-03-17 18:01:18 +0000

    1- Configured crypto-js library, 2- In Blockchain class, modified constructor to included new genesis block, 3- Modified addBlock function to include generation of our block hash.

--- simpleChain.js
+++ simpleChain.js
@@ -1,21 +1,28 @@
-// Class with a constructor for the block.
+const SHA256 = require('crypto-js/sha256');
 
+// Class with a constructor for the block.
 class Block {
   constructor(data) {
     this.hash = '';
     this.height = 0;
     this.body = data;
     this.time = 0;
-    this.previousblockhash = '';
+    this.previousBlockHash = '';
   }
 }
 
+// Class with a constructor for the Blockchain.
 class Blockchain {
   constructor() {
     this.chain = [];
+    this.addBlock(new Block('First block in the chain - Genesis block'));
   }
 
   addBlock(newBlock) {
+    if (this.chain.length > 0) {
+      newBlock.previousBlockHash = this.chain[this.chain.length - 1].hash; // here we find the hash of the previous block in the chain and set it to the previousBlockHash property of newBlock
+    }
+    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
     this.chain.push(newBlock);
   }
 }
